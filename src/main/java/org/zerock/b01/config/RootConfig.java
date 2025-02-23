320fdc8c20a9bccc19ecf6f20fb47d4801b5e2d7
leejinwon-bite <prosperity88897@naver.com>
2025-02-23 17:54:10 +0900

    fdbd

--- src/main/java/org/zerock/b01/config/RootConfig.java
+++ src/main/java/org/zerock/b01/config/RootConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RootConfig {
 
-    // config
+    // configcxvxccx
 
     @Bean
     public ModelMapper getMapper() {
