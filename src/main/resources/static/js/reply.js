45be49be6f9515bdeba14dbb25e062da77711041
beming! <68454472+leejinwon-bite@users.noreply.github.com>
2025-02-26 18:15:42 +0900

    tagName, page initial value

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -34,3 +34,13 @@ async function addReply(replyObj) {
 
     return response.data
 }
+
+async function getReply(rno) {
+    const response = await axios.get(`/replies/${rno}`)
+    return response.data
+}
+
+async function modifyReply(replyObj) {
+    const response = await axios.put(`/replies/${replyObj.rno}`, replyObj)
+    return response.data
+}
